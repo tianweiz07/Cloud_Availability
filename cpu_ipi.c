c9d4480800c786f8ff25045a01ff6364a8786c89
root <root@vm1.novalocal>
2014-04-23 16:12:36 +0000

    new

--- cpu_ipi.c
+++ cpu_ipi.c
@@ -11,7 +11,8 @@ static int __init cpu_ipi_init(void){
 	int attacker_cpu;
 	int victim_cpu;
 	
-	int current_time;
+	unsigned long current_time;
+	unsigned long next_time;
 
 	printk("entering cpu_ipi module\n");
 
@@ -24,13 +25,13 @@ static int __init cpu_ipi_init(void){
 	victim_cpu = (attacker_cpu + 1) % total_cpus;
 	printk("victim_cpu: %d\n", victim_cpu);	
 	current_time = jiffies;
-	printk("current_time: %d\n", current_time);	
-	while (time_before(jiffies, current_time+60*HZ)) {
-//		mdelay(1000);
+	next_time = current_time + 10*HZ;
+	printk("current_time: %lu\n", current_time);	
+	printk("next_time: %lu\n", next_time);	
+	while (time_before(jiffies, next_time)) {
+		mdelay(1000);
 		apic->send_IPI_mask(get_cpu_mask(victim_cpu), RESCHEDULE_VECTOR);
 	}
-	current_time = jiffies;
-	printk("current_time: %d\n", current_time);	
 
 
 //	set_cpus_allowed_ptr(current, get_cpu_mask(new_cpu));
