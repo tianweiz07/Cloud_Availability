1bf0b1b7d330cd5512178ec5eceaccbd54549bbd
Tianwei Zhang <tianweiz@EE-Desktop.(none)>
2014-04-25 00:55:50 -0400

    new

--- cpu_exe.c
+++ cpu_exe.c
@@ -9,12 +9,18 @@
 static int __init cpu_exe_init(void){
 	int num = 0;
 	int exe_cpu = 1;
+
+	unsigned long current_time;
+	unsigned long next_time;
 	
 	printk("entering cpu_exe module\n");
 
 	set_cpus_allowed_ptr(current, get_cpu_mask(exe_cpu));
 
-	while (1) {
+	current_time = jiffies;
+	next_time = current_time + 600*HZ;
+
+	while (time_before(jiffies, next_time)) {
 		num ++;
 		num --;
 	}
