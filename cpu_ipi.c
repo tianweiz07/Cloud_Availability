948b3789323eb89988c768347607d32fb90156e4
root <root@nebula5.(none)>
2014-04-29 22:02:07 -0400

    new

--- cpu_ipi.c
+++ cpu_ipi.c
@@ -1,6 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/kthread.h>
 #include <linux/module.h>
+#include <linux/unistd.h>
 #include <linux/cpumask.h>
 #include <linux/sched.h>
 #include <linux/delay.h>
@@ -11,18 +12,22 @@
 #define SLICE 15
 #define FREQUENCY 3292606
 
+#ifdef __i386
+__inline__ uint64_t rdtsc() {
+	uint64_t x;
+	__asm__ volatile ("rdtsc" : "=A" (x));
+	return x;
+}
+#elif __amd64
+__inline__ uint64_t rdtsc() {
+	uint64_t a, d;
+	__asm__ volatile ("rdtsc" : "=a" (a), "=d" (d));
+	return (d<<32) | a;
+}
+#endif;
+
 struct task_struct *cpu_exe_task;
 struct task_struct *cpu_ipi_task;
-
-unsigned long GetCycle(void){
-	unsigned long higher, lower;
-	unsigned long counter;
-	__asm__ ("rdtsc":"=a"(lower), "=b"(higher));
-	counter = higher;
-	counter = (higher << 32) + lower;
-	return counter;
-}
-
 
 int cpu_exe(void *ptr){
 	unsigned long long credit = 0;
@@ -51,28 +56,18 @@ int cpu_ipi(void *ptr){
 
 	unsigned long current_time;
 	unsigned long next_time;
-	unsigned long tsc;
-
+	unsigned long long tsc;
 
 	set_cpus_allowed_ptr(current, get_cpu_mask(send_cpu));
 
 	current_time = jiffies;
 	next_time = current_time + INTERVAL*HZ;
 
-//	while (taime_before(jiffies, next_time)) {
-//		tsc = GetCycle() + (unsigned long)(SLICE*FREQUENCY);
-		tsc = GetCycle();
-		printk("1tsc: %lu\n", tsc);
-		mdelay(1);
-		tsc = GetCycle();
-		printk("2tsc: %lu\n", tsc);
-		mdelay(1);
-		tsc = GetCycle();
-		printk("3tsc: %lu\n", tsc);
-		
+	while (taime_before(jiffies, next_time)) {
+		tsc = rdtsc() + SLICE*FREQUENCY;
 //		apic->send_IPI_mask(get_cpu_mask(recv_cpu), RESCHEDULE_VECTOR);
-//		while (GetCycle() < tsc);
-//	}
+		while (rdtsc() < tsc);
+	}
 	
 	printk("CPU INTERRUPTION DONE!\n");
 	return 0;
