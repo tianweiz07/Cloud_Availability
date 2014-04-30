f1fb9163989e78dbb0fdd8207432c336f11c1b25
root <root@victim.(none)>
2014-04-29 22:31:53 -0400

    new

--- cpu_ipi.c
+++ cpu_ipi.c
@@ -13,33 +13,29 @@
 #define FREQUENCY 3292606
 
 #ifdef __i386
-__inline__ uint64_t rdtsc() {
+__inline__ uint64_t rdtsc(void) {
 	uint64_t x;
 	__asm__ volatile ("rdtsc" : "=A" (x));
 	return x;
 }
 #elif __amd64
-__inline__ uint64_t rdtsc() {
+__inline__ uint64_t rdtsc(void) {
 	uint64_t a, d;
 	__asm__ volatile ("rdtsc" : "=a" (a), "=d" (d));
 	return (d<<32) | a;
 }
-#endif;
+#endif
 
-struct task_struct *cpu_exe_task;
-struct task_struct *cpu_ipi_task;
 
 int cpu_exe(void *ptr){
-	unsigned long long credit = 0;
+	uint64_t credit = 0;
 	int exe_cpu = 1;
 
-	unsigned long current_time;
 	unsigned long next_time;
 
 	set_cpus_allowed_ptr(current, get_cpu_mask(exe_cpu));
 
-	current_time = jiffies;
-	next_time = current_time + INTERVAL*HZ;
+	next_time = jiffies + INTERVAL*HZ;
 
 	while (time_before(jiffies, next_time)) {
 		credit ++;
@@ -54,31 +50,33 @@ int cpu_ipi(void *ptr){
 	int send_cpu = 0;
 	int recv_cpu = 1;
 
-	unsigned long current_time;
 	unsigned long next_time;
-	unsigned long long tsc;
+	uint64_t tsc;
 
 	set_cpus_allowed_ptr(current, get_cpu_mask(send_cpu));
 
-	current_time = jiffies;
-	next_time = current_time + INTERVAL*HZ;
+	next_time = jiffies + INTERVAL*HZ;
 
-	while (taime_before(jiffies, next_time)) {
+	while (time_before(jiffies, next_time)) {
 		tsc = rdtsc() + SLICE*FREQUENCY;
-//		apic->send_IPI_mask(get_cpu_mask(recv_cpu), RESCHEDULE_VECTOR);
+		apic->send_IPI_mask(get_cpu_mask(recv_cpu), RESCHEDULE_VECTOR);
 		while (rdtsc() < tsc);
 	}
 	
 	printk("CPU INTERRUPTION DONE!\n");
+
 	return 0;
 }
 
 
 static int __init cpu_ipi_init(void){
+	struct task_struct *cpu_exe_task;
+	struct task_struct *cpu_ipi_task;
 
 	printk("entering cpu_ipi module\n");
-//	cpu_exe_task = kthread_run(&cpu_exe, NULL, "CPU EXECUTION");
+	cpu_exe_task = kthread_run(&cpu_exe, NULL, "CPU EXECUTION");
 	cpu_ipi_task = kthread_run(&cpu_ipi, NULL, "CPU INTERRUPTION");
+
 	return 0;
 }
 
