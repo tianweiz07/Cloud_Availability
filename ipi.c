210f37074a1e5c3263249993fdbbbce0b55ba622
root <root@nebula5.1>
2014-05-30 11:04:07 -0400

    new

--- ipi.c
+++ ipi.c
@@ -13,14 +13,14 @@
 #include <asm/xen/hypervisor.h>
 
 //unit: millisecond
-#define INTERVAL1 8 
-#define INTERVAL2 10
+#define INTERVAL1 1 
+#define INTERVAL2 4
 
-#define ROUND_NUM 100
+#define ROUND_NUM 20
 
 // Check /proc/kallsyms to get the following addresses
 #define IPI_ADDR 0xffffffff813af770
-//#define AffINITY_ADDR 0xffffffff81063580
+#define AFFINITY_ADDR 0xffffffff81063580
 
 // iscpu to get the CPU frequency
 #define FREQUENCY 3292600
@@ -46,31 +46,33 @@ __inline__ uint64_t rdtsc(void) {
 atomic_t round_count;
 
 void (*xen_send_IPI_one)(unsigned int cpu, enum ipi_vector vector) = IPI_ADDR;
-//long (*sched_setaffinity)(pid_t pid, const struct cpumask *in_mask) = AFFINITY_ADDR;
+long (*m_sched_setaffinity)(pid_t pid, const struct cpumask *in_mask) = AFFINITY_ADDR;
 
 void set_affinity(unsigned int cpu) {
 	struct task_struct *m_task, *m_thread;
 	for_each_process(m_task) {
-		sched_setaffinity(m_task->pid, cpumask_of(cpu));
+		m_sched_setaffinity(m_task->pid, cpumask_of(cpu));
 		list_for_each_entry(m_thread, &m_task->thread_group, thread_group) {
-			sched_setaffinity(m_thread->pid, cpumask_of(cpu));
+			m_sched_setaffinity(m_thread->pid, cpumask_of(cpu));
 		}
 	}
 }
 
 void working_thread(void *ptr) {
 	uint64_t tsc;
-
+	uint64_t credit;
+	printk("Running working_thread @ vcpu %d\n", smp_processor_id());
 	tsc = rdtsc() + INTERVAL1 * FREQUENCY;
 
 	while (rdtsc() < tsc)
-		printk("do something\n");
+		credit ++;
 	return;
 }
 
 int loop(void *ptr) {
 	int val;
 	unsigned long flags;
+	printk("Entering loop function\n");
 	set_cpus_allowed_ptr(current, cpumask_of(RECV_CPUID));
 
 	local_irq_save(flags);
@@ -93,15 +95,20 @@ int loop(void *ptr) {
 
 static int __init ipi_init(void) {
 	uint64_t tsc;
-
 	printk("Entering ipi module\n");
 	set_affinity(SEND_CPUID);
 	set_cpus_allowed_ptr(current, cpumask_of(SEND_CPUID));
-	atomic_set(&round_count, 0);
+	atomic_set(&round_count, -1);
+
 	kernel_thread(loop, NULL, 0);
+
+	while (atomic_read(&round_count) < 0)
+		schedule();
+
 	
 	while (atomic_read(&round_count) < ROUND_NUM) {
 		tsc = rdtsc() + INTERVAL2 * FREQUENCY;
+		printk("Sending IPI from vcpu %d\n", smp_processor_id());
 		xen_send_IPI_one(RECV_CPUID, XEN_CALL_FUNCTION_VECTOR);
 		while (rdtsc() < tsc);
 	}
