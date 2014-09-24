210cbbc53c9a17349d4f576176febe0a0c4268f6
root <root@ubuntu.(none)>
2014-09-24 11:49:12 -0400

    tianweiz: bus locking

--- BUS/bus_lock.c
+++ BUS/bus_lock.c
@@ -10,23 +10,14 @@
 #endif
 
 // Cache configurations
-#define CACHE_SET_NR 30720
+#define CACHE_SET_NR 64
 #define CACHE_WAY_NR 8
 #define CACHE_LINE_SIZE 64
 #define CACHE_SIZE (CACHE_LINE_SIZE*CACHE_WAY_NR*CACHE_SET_NR)
 
 // Page parameters
-#define PAGE_COLOR (CACHE_SET_NR*CACHE_LINE_SIZE/PAGE_SIZE)
-#define PAGE_PTR_NR 6
-#define PAGE_ORDER 10
-#define PAGE_NR 1024
 
 // Timing parameters
-#define SCAN_NR 10000
-#define INTERVAL1 10 // unit: s, for the whole program
-#define INTERVAL2 1 // unit: ms, for the cache scan interval
-#define FREQUENCY 2900048
-
 #ifdef __i386
 __inline__ uint64_t rdtsc(void) {
 	uint64_t x;
@@ -45,28 +36,38 @@ __inline__ uint64_t rdtsc(void) {
 static uint64_t SYS_CALL_TABLE_ADDR = 0xffffffff81801320;
 #define __NR_CachePrime 188
 
+/*
 static noinline void fetch_and_add(int *variable, int value) {
 	asm volatile ("lock; xaddl %%eax, %2\n\t"
-		      "clflush (%1)\n\t"
 		      :"=a" (value)
 		      :"a" (value), "m"(*variable)
 		      :"memory");
 }
+*/
 
-unsigned long cache_pg[PAGE_COLOR][CACHE_WAY_NR];
-int cache_idx[PAGE_COLOR];
-struct page *ptr[PAGE_PTR_NR];
+/*
+static noinline void cache_flush(char *address) {
+	__asm__ volatile ("clflush (%0)"
+			  :
+			  :"r"(address)
+			  :"memory");
+}
+*/
+
+static noinline void flush_fetch(char *flush_address, int *read_address, int value) {
+	asm volatile ("clflush (%1)\n\t"
+		      "clflush (%2)\n\t"
+		      "lock; xaddl %%eax, %3\n\t"
+		      :"=a"(value)
+		      :"r"(flush_address), "r"(flush_address+CACHE_LINE_SIZE), "m"(*read_address), "a"(value)
+		      :"memory");
+}
+
+struct page *ptr;
+char *cache_list[8];
 
 unsigned long page_to_virt(struct page* page){
 	return (unsigned long)phys_to_virt(page_to_phys(page));
-}
-
-int page_to_color(struct page* page) {
-	return pfn_to_mfn(page_to_pfn(page))%PAGE_COLOR;
-}
-
-int virt_to_color(unsigned long virt) {
-	return pfn_to_mfn(virt_to_pfn((unsigned long)virt))%PAGE_COLOR;
 }
 
 void SetPageRW(struct page* page)
@@ -99,80 +100,31 @@ void SetPageRO(struct page* page)
 
 
 void CacheInit(void) {
-	int i, j, c;
-	for (i=0; i<PAGE_PTR_NR; i++)
-		ptr[i] = alloc_pages(GFP_KERNEL, PAGE_ORDER);
+	int i, j, k;
+	ptr = alloc_pages(GFP_KERNEL, 3);
+	for(i=0;i<8;i++)
+		cache_list[i] = (char*)phys_to_virt(page_to_phys(ptr+i));
 
-	for(i=0; i<PAGE_COLOR; i++) {
-		cache_idx[i] = 0;
-		for (j=0; j<CACHE_WAY_NR; j++)
-			cache_pg[i][j] = 0;
-	}
-
-	for (i=0; i<PAGE_PTR_NR; i++) {
-		for (j=0; j<PAGE_NR; j++) {
-			c = page_to_color(ptr[i] + j);
-			if (cache_idx[c] < CACHE_WAY_NR) {
-				cache_pg[c][cache_idx[c]] = page_to_virt(ptr[i] + j);
-				cache_idx[c] ++;
+	for (i=0; i<8; i++) {
+		for (j=0; j<64; j++) {
+			for (k=0; k<64; k++) {
+				cache_list[i][j*64+k] = k;
 			}
 		}
 	}
+
 	return;
 }
 
 void CacheFree(void) {
-	int i;
-	for (i=0; i<PAGE_PTR_NR; i++)
-		__free_pages(ptr[i], PAGE_ORDER);
+	__free_pages(ptr, 3);
 }
 
-int CacheCheck(void) {
-	int i, j;
-	int val = 1;
-	for (i=0; i<PAGE_COLOR; i++)
-		for (j=0; j<CACHE_WAY_NR; j++)
-			if (i != virt_to_color(cache_pg[i][j]))
-				val = 0;
-	return val;
-}
-
-static noinline unsigned long CacheScan(int cpu_id) {
-	int i, j, p, q;
-	unsigned long temp = 0;
-	uint64_t tsc;
-	
-	tsc = rdtsc();
-	for (i=cpu_id*CACHE_SET_NR/4; i<(cpu_id+1)*CACHE_SET_NR/4; i++) {
-		for (j=0; j<CACHE_WAY_NR; j++) {
-			p = i / (PAGE_SIZE/CACHE_LINE_SIZE);
-			q = i % (PAGE_SIZE/CACHE_LINE_SIZE);
-			temp = *(unsigned long*)(cache_pg[p][j] + q * CACHE_LINE_SIZE);
-		}
-	}
-
-	return temp;
-}
-
-asmlinkage void sys_CachePrime(int t, int cpu_id) {
-	int k;
-	int val;
-	unsigned long tsc1;
-
-	set_cpus_allowed_ptr(current, cpumask_of(cpu_id));
-
-	val = CacheCheck();
-	if (!val) {
-		printk("Cache Initialization Error\n");
-		return;
-	}
-	else {
-		printk("Cache Initialization Correct\n");
-		tsc1 = jiffies + t * HZ;
-		while (time_before(jiffies, tsc1)) {
-			for (k=0; k<2000; k++) 
-				CacheScan(cpu_id);
-		}
+asmlinkage void sys_CachePrime(int t) {
+	unsigned long tsc;
+	tsc = jiffies + t * HZ;
+	while (time_before(jiffies, tsc)) {
+		flush_fetch(cache_list[0], (int *)(cache_list[0]+CACHE_LINE_SIZE-1), 0x0);
 	}
 	return;
 }
--- BUS/test.c
+++ BUS/test.c
@@ -4,35 +4,10 @@
 #include <sys/syscall.h>
 
 #define __NR_CachePrime 188
+#define TIME 10
 
 int main(int argc, char* argv[]) {
-	int t = 500;
-	pid_t pid1, pid2;
-	pid1 = fork();
-	pid2 = fork();
-	if ((pid1 < 0)||(pid2 < 0)) {
-		printf("Fork Error\n");
-		return 0;
-	}
-	if ((pid1 == 0)&&(pid2 == 0)) {
-		syscall(__NR_CachePrime, t, 0);
-	}
-
-	if ((pid1 == 0)&&(pid2 > 0)) {
-		usleep(20000);
-		syscall(__NR_CachePrime, t, 1);
-	}
-
-	if ((pid1 > 0)&&(pid2 == 0)) {
-		usleep(40000);
-		syscall(__NR_CachePrime, t, 2);
-	}
-
-	if ((pid1 > 0)&&(pid2 > 0)) {
-		usleep(60000);
-		syscall(__NR_CachePrime, t, 3);
-	}
-
-	printf("Cache Prime Done!\n");
+	syscall(__NR_CachePrime, TIME);
+	printf("Bus Lock Done!\n");
 	return 0;
 }
