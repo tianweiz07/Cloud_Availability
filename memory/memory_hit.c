0d57ff8ecd526b1469b20373b46e567c84268029
root <root@ubuntu.(none)>
2014-10-19 22:06:58 -0400

    tianweiz

--- memory/memory_hit.c
+++ memory/memory_hit.c
@@ -1,6 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/random.h>
 #include <asm/io.h>
 #include <asm/xen/page.h>
 #include <asm/xen/hypercall.h>
@@ -11,6 +12,9 @@
 
 // Cache configurations
 #define CACHE_LINE_SIZE 64
+#define PAGE_PTR_NR 6
+#define PAGE_ORDER 10
+#define PAGE_NR 1024
 
 // Timing parameters
 #ifdef __i386
@@ -29,27 +33,34 @@ __inline__ uint64_t rdtsc(void) {
 
 // Syscall information
 static uint64_t SYS_CALL_TABLE_ADDR = 0xffffffff81801320;
-#define __NR_BusLock 188
+#define __NR_MemoryHit 188
 
-/*
-static noinline void cache_flush(char *address) {
+
+static noinline void cache_flush(uint32_t *address) {
 	__asm__ volatile ("clflush (%0)"
 			  :
 			  :"r"(address)
 			  :"memory");
 }
-*/
 
-static noinline void atomic_fetch(int *read_address, int value) {
+
+static noinline void uncache_access(uint32_t *read_address, uint32_t value) {
 	asm volatile (
-		      "lock; xaddl %%eax, %1\n\t"
-		      :"=a"(value)
-		      :"m"(*read_address), "a"(value)
-		      :"memory");
+		      "mov %0, %%r9\n\t"
+		      "mov %1, %%ecx\n\t"
+		      "movnti %%ecx, (%%r9)\n\t"
+		      "sfence\n\t"
+		      :
+		      :"r"(read_address), "r"(value)
+		      :
+		      );
+	return;
 }
 
-struct page *ptr;
-char *cache_list;
+
+
+struct page *ptr[PAGE_PTR_NR];
+uint32_t *cache_list[PAGE_PTR_NR][PAGE_NR];
 
 unsigned long page_to_virt(struct page* page){
 	return (unsigned long)phys_to_virt(page_to_phys(page));
@@ -85,21 +96,38 @@ void SetPageRO(struct page* page)
 
 
 void CacheInit(void) {
-	ptr = alloc_pages(GFP_KERNEL, 1);
-	cache_list = (char*)phys_to_virt(page_to_phys(ptr));
+	int i, j, k;
+
+	for (i=0; i<PAGE_PTR_NR; i++) {
+		ptr[i] = alloc_pages(GFP_KERNEL, PAGE_ORDER);
+		for (j=0; j<PAGE_NR; j++)
+			cache_list[i][j] = (uint32_t*)phys_to_virt(page_to_phys(ptr[i] + j));
+	}
+
+	for (i=0; i<PAGE_PTR_NR; i++)
+		for (j=0; j<PAGE_NR; j++)
+			for (k=0; k<1024; k++)
+				cache_flush(&cache_list[i][j][k]);
 	return;
 }
 
 void CacheFree(void) {
-	__free_pages(ptr, 1);
+	int i;
+	for (i=0; i<PAGE_PTR_NR; i++)
+		__free_pages(ptr[i], PAGE_ORDER);
 }
 
-asmlinkage void sys_BusLock(int t, int cpu_id) {
+asmlinkage void sys_MemoryHit(int t) {
 	unsigned long tsc;
+	int i, j, k;
 	tsc = jiffies + t * HZ;
 	while (time_before(jiffies, tsc)) {
-		atomic_fetch((int *)(cache_list + (cpu_id + 1) * CACHE_LINE_SIZE - 1), 0x0);
+		for (i=0; i<PAGE_PTR_NR; i++) 
+			for (j=0; j<PAGE_NR; j++)
+				for (k=0; k<1024; k++)
+					uncache_access(&cache_list[i][j][k], 0x9);
 	}
+
 	return;
 }
 
@@ -113,8 +141,8 @@ int __init init_addsyscall(void) {
 	printk("Module Init\n");
 	my_sys_call_table = (void**) SYS_CALL_TABLE_ADDR;
 	SetPageRW(virt_to_page(my_sys_call_table));
-	position_collect = (uint64_t(*)(void))(my_sys_call_table[__NR_BusLock]);
-	my_sys_call_table[__NR_BusLock] = sys_BusLock;
+	position_collect = (uint64_t(*)(void))(my_sys_call_table[__NR_MemoryHit]);
+	my_sys_call_table[__NR_MemoryHit] = sys_MemoryHit;
 	SetPageRO(virt_to_page(my_sys_call_table));
 	CacheInit();
 	return 0;
@@ -123,7 +151,7 @@ int __init init_addsyscall(void) {
 void __exit exit_addsyscall(void) {
 	printk("Module Exit\n");
 	SetPageRW(virt_to_page(my_sys_call_table));
-	my_sys_call_table[__NR_BusLock] = position_collect;
+	my_sys_call_table[__NR_MemoryHit] = position_collect;
 	SetPageRO(virt_to_page(my_sys_call_table));
 	CacheFree();
 	return;
