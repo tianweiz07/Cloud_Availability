9bcf239844a76fff860e680405bd9e08c787fef1
tianweiz07 <tianweiz@princeton.edu>
2014-12-29 16:58:01 -0500

    Update diff.py

--- cache_l3/diff.py
+++ cache_l3/diff.py
@@ -23,22 +23,22 @@ def process():
 	for i in range(2048):
 		first0 = int(activities1[i].split(' ')[0])
 		second0 = int(activities2[i].split(' ')[0])
-		diff0 = second0 - first0
+		diff0 = first0 - second0
 		first1 = int(activities1[i].split(' ')[1])
 		second1 = int(activities2[i].split(' ')[1])
-		diff1 = second1 - first1
+		diff1 = first1 - second1
 		first2 = int(activities1[i].split(' ')[2])
 		second2 = int(activities2[i].split(' ')[2])
-		diff2 = second2 - first2
+		diff2 = first2 - second2
 		first3 = int(activities1[i].split(' ')[3])
 		second3 = int(activities2[i].split(' ')[3])
-		diff3 = second3 - first3
+		diff3 = first3 - second3
 		first4 = int(activities1[i].split(' ')[4])
 		second4 = int(activities2[i].split(' ')[4])
-		diff4 = second4 - first4
+		diff4 = first4 - second4
 		first5 = int(activities1[i].split(' ')[5])
 		second5 = int(activities2[i].split(' ')[5])
-		diff5 = second5 - first5
+		diff5 = first5 - second5
 		
 		data3.write("%d %d %d %d %d %d\n" % (diff0, diff1, diff2, diff3, diff4, diff5))
 	data3.close()
