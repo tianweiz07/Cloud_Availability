eb4aa3aec8597c4b54cc76a56bbba721e7749394
Palms Admin <palms_admin@nebula1-2.(none)>
2015-04-17 15:26:05 -0400

    new

--- colocation/receiver.c
+++ colocation/receiver.c
@@ -1,40 +1,30 @@
-#define _POSIX_C_SOURCE 199309
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
-#include <sched.h>
+#endif
+
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
+#include <sched.h>
+#include <pthread.h>
+#include <sys/syscall.h>
 #include <unistd.h>
 #include <inttypes.h>
 #include <fcntl.h>
 #include <time.h>
-#include <pthread.h>
-#include <sys/mman.h>
-#include <sys/syscall.h>
 #include <sys/time.h>
+#include <sys/mman.h>
 #include <sys/resource.h>
+#include <sched.h>
 #include <sys/types.h>
 
 #define line_size 64
-#define size 1048576
 
-char *data_array;
+int *array;
+int *target_array;
+int *index_array;
 
-int main (int argc, char *argv[]) {
-	int interval = atoi(argv[1]);
-
-        int fd = open("nebula2", O_RDWR, 0755);
-        if (fd < 0) {
-                printf("file open error!\n");
-                return 0;
-        }
-        data_array = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
-
-        if (data_array == MAP_FAILED) {
-                printf("map 1 error!\n");
-                unlink("nebula2");
-                return 0;
-        }
-
+int main(int argc, char *argv[]) {
 
         cpu_set_t set;
         CPU_ZERO(&set);
@@ -44,25 +34,47 @@ int main (int argc, char *argv[]) {
                 return 0;
         }
 
-	int *read_address;
-	read_address = (int *)(data_array + line_size - 1);
-	int value = 0x0;
-	int index;
+	int mem_size = 1048576;
+	int interval = atoi(argv[1]);
+	int size = atoi(argv[2])/2;
 
-        time_t finish_time;
-	while (1) {
-                index = 0;
-                finish_time = time(NULL) + interval;
-                while(time(NULL)<finish_time) {
-                        index ++;
-       		 	__asm__(
-				"lock; xaddl %%eax, %1\n\t"
-                   	        :"=a"(value)
-                       	        :"m"(*read_address), "a"(value)
-	                        :"memory");
-		}
-                printf("%d\n", index);
+        int fd = open("nebula1", O_RDWR, 0755);
+        if (fd < 0) {
+                printf("file open error!\n");
+                return 0;
         }
+        array = mmap(NULL, mem_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+        if (array == MAP_FAILED) {
+                printf("map 1 error!\n");
+                unlink("nebula1");
+                return 0;
+        }
+
+	target_array = &array[size/sizeof(int)];
+
+	index_array = (int *)malloc(size/line_size*sizeof(int));
+
+	int i, j;
+	for (i=0; i<size/line_size; i++)
+		index_array[i] = i*line_size/sizeof(int);
+
+	for (i=0; i <size/sizeof(int); i++) {
+		array[i] = rand();
+		target_array[i] = rand();
+	}
+
+	time_t finish_time;
+	int index;
+	while (1) {
+		index = 0;
+		finish_time = time(NULL) + interval;
+		while(time(NULL)<finish_time) {
+			index ++;
+			for (j=0; j < size/line_size; j++)
+				target_array[index_array[j]] = array[index_array[j]];
+		}
+		printf("%d\n", index);
+	}
 
 	return 0;
 }
